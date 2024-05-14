d95e74ca1f8d867591c95a1d5e6df56e169d5118
Victor <strecmit@gmail.com>
2024-05-15 00:58:45 +0200

    2

--- sprav.py
+++ sprav.py
@@ -10,7 +10,18 @@ def menu():
     return(vybor)
 
 def search():
+    poisk = input("ВВЕДИТЕ ФАМИЛИЮ/ИМЯ/ОТЧЕСТВО/№ ТЕЛЕФОНА >")
+    with open("sprav.txt","a", encoding="utf-8") as file:
+        lines = 0
+        words1 = 0
+        for line in file:
+            if poisk in line:
+                print(line)
+    return()
+
+def new_c():
     
+        
+        
 
 
-print(menu())
