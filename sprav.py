2d99e97124c90577af32acfd29aa013e2acf7334
Victor <strecmit@gmail.com>
2024-05-15 00:08:06 +0200

    menu

---
+++ sprav.py
@@ -0,0 +1,16 @@
+print("***ТЕЛЕФОННЫЙ СПРАВОЧНИК 1.0***")
+
+def menu():
+    print("1-ПОИСК")
+    print("2-НОВЫЙ КОНТАКТ")
+    print("3-РЕДАКТОР")
+    print("4-ВЫХОД")
+    vybor = input("ВАШ ВЫБОР >")
+
+    return(vybor)
+
+def search():
+    
+
+
+print(menu())
